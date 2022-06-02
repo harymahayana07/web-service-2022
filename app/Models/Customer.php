62c523aaebb6f9dd05615fa59c361bb6271eccf8
harymahayana07 <harymahayana@gmail.com>
2022-06-02 21:02:06 +0800

    Update Ulang Api

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,8 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
-    protected $table = 'customer';
-    protected $primaryKey = 'id';
-    public $timestamps = true;
-    protected $fillable = ['id', 'name', 'phone', 'email', 'address'];
+   
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\API\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('v1/customer', [CustomerController::class, 'index']);
-Route::get('v1/customer/{id}', [CustomerController::class, 'show']);
+
 
